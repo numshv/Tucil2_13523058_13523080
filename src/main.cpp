bd2c94c443d819a817764688eba8f999c8bc1e03
numshv <noumisyifa@gmail.com>
2025-04-04 20:59:21 +0700

    feat: add time tracking

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #include "Utils.hpp"
+#include <chrono>
 
 int main(){
     string inputImagePath, errorMethod, outputImagePath;
@@ -11,8 +12,18 @@ int main(){
     // Handle input and process the image
     inputHandler(inputImagePath, image, errorMethod, threshold, minBlockSize, targetCompression, outputImagePath);
 
+    auto start = chrono::high_resolution_clock::now();
+
+    //TODO: Implement rekursi di sini
+
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    // cout << "Execution time: " << duration.count() << " ms" << endl;
+
     // Just to test if it's possible to modify and save it to a new file
     saveModifiedImage(image, outputImagePath);
+
+    //TODO: output handler
 
     return 0;
 }
