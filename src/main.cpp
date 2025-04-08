12a35bbf243d136f3a3bc409b0af4c770a7497b7
numshv <noumisyifa@gmail.com>
2025-04-08 14:33:47 +0700

    docs: clean up comments

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,28 @@ int main(){
     float threshold, targetCompression;
     int minBlockSize;
 
-    // Handle input and process the image
+    // Handle input validations and process the image into 2d vector of rgb
     inputHandler(inputImagePath, image, errorMethod, threshold, minBlockSize, targetCompression, outputImagePath);
 
-    // Start menghitung waktu eksekusi
+    // Start the timer
     auto start = chrono::high_resolution_clock::now();
 
-    //Implementasi rekursi di sini
+    //Start of quadtree recursion compression 
+
     QuadTree qt;
     qt.buildTree(image, threshold, errorMethod, minBlockSize);
     qt.reconstructImage(image);
 
-    // Rekursi selesai
+    //End of quadtree recursion compression 
+
     auto end = chrono::high_resolution_clock::now();
+    // Calculate the duration
     auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
 
-    // Just to test if it's possible to modify and save it to a new file
+    // Save the image to the path
     saveCompressedImage(image, outputImagePath);
 
+    // Print the output
     outputHandler(outputImagePath, inputImagePath, qt, duration);
 
     return 0;
